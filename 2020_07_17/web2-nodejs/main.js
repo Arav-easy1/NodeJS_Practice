c7b6a066aac027d7b7a30581ccb5d8d613b8aa93
EASY_ARAV <wg0418@gmail.com>
2020-07-17 18:11:01 +0900

    readfile의 원리 알게됨.

--- 2020_07_17/web2-nodejs/main.js
+++ 2020_07_17/web2-nodejs/main.js
@@ -1,16 +1,46 @@
 var http = require('http');
 var fs = require('fs');
+var url = require('url');
+
 var app = http.createServer(function(request,response){
-    var url = request.url;
-    if(request.url == '/'){
-      url = '/index.html';
+    var _url = request.url;
+    var queryData = url.parse(_url, true).query;
+    var title = queryData.id;
+    if(_url == '/'){
+      title = 'Welcome';
     }
-    if(request.url == '/favicon.ico'){
+    if(_url == '/favicon.ico'){
       return response.writeHead(404);
     }
     response.writeHead(200);
-    console.log(__dirname + url);
-    response.end(fs.readFileSync(__dirname + url));
+    fs.readFile(`./data/${queryData.id}`, 'utf8', function(err, description){
+      var template = `
+      <!doctype html>
+      <html>
+      <head>
+        <title>WEB1 - ${title}</title>
+        <meta charset="utf-8">
+      </head>
+      <body>
+        <h1><a href="/">WEB</a></h1>
+        <ul>
+          <li><a href="/?id=HTML">HTML</a></li>
+          <li><a href="/?id=CSS">CSS</a></li>
+          <li><a href="/?id=JavaScript">JavaScript</a></li>
+        </ul>
+        <h2>${title}</h2>
+        <p>${description}</p>
+      </body>
+      </html>`;
+
+      // 아래 부분이 내가 가지고 있는 파일들을 불러주는 것이었음.
+      // response.end(fs.readFileSync(__dirname + _url));
+      response.end(template);
+    })
+
 
 });
 app.listen(3000);
+
+// http://localhost/?id=HTML 라는 URL이 있다면,
+// ? 이후에 나오는 부분들을 Query String 이라고 한다.
