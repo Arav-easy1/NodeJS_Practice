6741c514c0a379c5a2c0609b27805ff262a50860
EASY_ARAV <wg0418@gmail.com>
2020-07-17 20:42:34 +0900

    데이터 추가되는대로 불러오기 가능.

--- 2020_07_17/web2-nodejs/main.js
+++ 2020_07_17/web2-nodejs/main.js
@@ -2,41 +2,95 @@ var http = require('http');
 var fs = require('fs');
 var url = require('url');
 
-var app = http.createServer(function(request,response){
-    var _url = request.url;
-    var queryData = url.parse(_url, true).query;
-    var title = queryData.id;
-    if(_url == '/'){
-      title = 'Welcome';
-    }
-    if(_url == '/favicon.ico'){
-      return response.writeHead(404);
-    }
-    response.writeHead(200);
-    fs.readFile(`./data/${queryData.id}`, 'utf8', function(err, description){
-      var template = `
-      <!doctype html>
-      <html>
-      <head>
-        <title>WEB1 - ${title}</title>
-        <meta charset="utf-8">
-      </head>
-      <body>
-        <h1><a href="/">WEB</a></h1>
-        <ul>
-          <li><a href="/?id=HTML">HTML</a></li>
-          <li><a href="/?id=CSS">CSS</a></li>
-          <li><a href="/?id=JavaScript">JavaScript</a></li>
-        </ul>
-        <h2>${title}</h2>
-        <p>${description}</p>
-      </body>
-      </html>`;
+var app = http.createServer(function(request, response) {
+  var _url = request.url;
+  var queryData = url.parse(_url, true).query;
+  var pathname = url.parse(_url, true).pathname;
 
-      // 아래 부분이 내가 가지고 있는 파일들을 불러주는 것이었음.
-      // response.end(fs.readFileSync(__dirname + _url));
-      response.end(template);
-    })
+  // if(_url == '/'){
+  //   title = 'Welcome';
+  // }
+  // if(_url == '/favicon.ico'){
+  //   return response.writeHead(404);
+  // }
+
+  if (pathname === "/") {
+    if (queryData.id === undefined) {
+
+      fs.readdir('./data', function(error, filelist) {
+        var title = "Welcome";
+        var description = "Hello, Node.js";
+
+        var list = '<ul>';
+
+        var i = 0;
+        while (i < filelist.length) {
+          list = list + `<li><a href="/?id=${filelist[i]}">${filelist[i]}</a></li>`;
+          i = i + 1;
+        }
+
+
+        list = list + '</ul>';
+
+        var template = `
+          <!doctype html>
+          <html>
+          <head>
+            <title>WEB1 - ${title}</title>
+            <meta charset="utf-8">
+          </head>
+          <body>
+            <h1><a href="/">WEB</a></h1>
+            ${list}
+            <h2>${title}</h2>
+            <p>${description}</p>
+          </body>
+          </html>`;
+        response.writeHead(200);
+        response.end(template);
+      })
+
+
+    } else {
+      fs.readdir('./data', function(error, filelist) {
+        var title = "Welcome";
+        var description = "Hello, Node.js";
+        var list = '<ul>';
+        var i = 0;
+        while (i < filelist.length) {
+          list = list + `<li><a href="/?id=${filelist[i]}">${filelist[i]}</a></li>`;
+          i = i + 1;
+        }
+        list = list + '</ul>';
+        fs.readFile(`./data/${queryData.id}`, 'utf8', function(err, description) {
+          var title = queryData.id;
+          var template = `
+        <!doctype html>
+        <html>
+        <head>
+          <title>WEB1 - ${title}</title>
+          <meta charset="utf-8">
+        </head>
+        <body>
+          <h1><a href="/">WEB</a></h1>
+          ${list}
+          <h2>${title}</h2>
+          <p>${description}</p>
+        </body>
+        </html>`;
+
+          // 아래 부분이 내가 가지고 있는 파일들을 불러주는 것이었음.
+          // response.end(fs.readFileSync(__dirname + _url));
+          response.writeHead(200); // 200이라는 숫자는 서버와 브라우저가 서로 ㅇㅋ 하고 주고 받는 약속의 숫자.
+          response.end(template);
+        });
+      });
+    }
+
+  } else {
+    response.writeHead(404); // 못찾겠다. 당연 404
+    response.end("Not Found");
+  }
 
 
 });
