b308387290347a5cf78858c4da0c140a025d91d4
EASY_ARAV <wg0418@gmail.com>
2020-07-17 16:28:04 +0900

    Template Literal 재밌다ㅎ

---
+++ 2020_07_17/syntax/template.js
@@ -0,0 +1,14 @@
+var name = 'ARAV';
+var letter = 'Dear '+name+'\n\nLorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.';
+// Template Literal
+
+// var a = 1; 은 숫자1을 표현한 리터럴
+// var a = '1'; 은 문자1을 표현한 리터럴
+
+// ~ 표시 아래에 있는 ``을 사용하여 Template Literal을 표현한다.
+var letter = `Dear ${name}
+
+Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.`;
+
+
+console.log(letter);
